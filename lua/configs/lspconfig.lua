34a5d3a6a7bceaf44b5361a6d6672a95136f5e84
Yutong Song <hiyutongsong@gmail.com>
2026-04-16 05:47:14 -0700

    fix dap: auto install codelldb

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,6 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "clangd", "verible", "pyright", "texlab" }
+local servers = { "clangd", "verible", "pyright", "texlab", "codelldb" }
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers 
