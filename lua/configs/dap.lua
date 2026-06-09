eee148a5a5480d8a107f1c032cc82261734c8068
Yutong Song <hiyutongsong@gmail.com>
2026-06-09 09:34:22 +0800

    regularize dap settings

--- lua/configs/dap.lua
+++ lua/configs/dap.lua
@@ -9,11 +9,14 @@ map('n', '<F1>', function() require('dap').step_over() end)
 map('n', '<F2>', function() require('dap').step_into() end)
 map('n', '<F3>', function() require('dap').step_out() end)
 map('n', '<Leader>b', function() require('dap').toggle_breakpoint() end)
+map('n', '<Leader>c', function()
+  require('dap').set_breakpoint(vim.fn.input('Breakpoint condition: '))
+end, { desc = 'DAP Conditional Breakpoint' })
 
-vim.api.nvim_set_hl(0, 'DapBreakpoint', { ctermbg = 0, fg = '#993939', bg = '#31353f' })
+vim.api.nvim_set_hl(0, 'DapBreakpoint', { ctermbg = 0, fg = '#993939', bg = '' })
 vim.api.nvim_set_hl(0, 'DapLogPoint', { ctermbg = 0, fg = '#61afef', bg = '#31353f' })
 vim.api.nvim_set_hl(0, 'DapStopped', { ctermbg = 0, fg = '#98c379', bg = '#31353f' })
-vim.fn.sign_define('DapBreakpoint', { text='●', texthl='DapBreakpoint', linehl='DapBreakpoint', numhl='DapBreakpoint' })
+vim.fn.sign_define('DapBreakpoint', { text='●', texthl='DapBreakpoint', linehl='', numhl='' })
 vim.fn.sign_define('DapBreakpointCondition', { text='ﳁ', texthl='DapBreakpoint', linehl='DapBreakpoint', numhl='DapBreakpoint' })
 vim.fn.sign_define('DapBreakpointRejected', { text='', texthl='DapBreakpoint', linehl='DapBreakpoint', numhl= 'DapBreakpoint' })
 vim.fn.sign_define('DapLogPoint', { text='', texthl='DapLogPoint', linehl='DapLogPoint', numhl= 'DapLogPoint' })
@@ -45,27 +48,4 @@ require("dap-python").setup("python")
 dap.adapters.codelldb = {
   type = "executable",
   command = "codelldb",
-
-  -- On windows you may have to uncomment this:
-  -- detached = false,
 }
-
-dap.configurations.cpp = {
-  {
-    name = "Launch file",
-    type = "codelldb",
-    request = "launch",
-    program = function()
-      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-    end,
-    args = function()
-      local args_string = vim.fn.input('Arguments: ')
-      return vim.split(args_string, " +")
-    end,
-    cwd = '${workspaceFolder}',
-    stopOnEntry = false,
-  },
-}
-
-dap.configurations.c = dap.configurations.cpp
-dap.configurations.rust = dap.configurations.cpp
